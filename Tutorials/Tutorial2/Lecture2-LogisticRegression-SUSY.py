0ace7a03e36057edede7119fbce79e7ec537909a
Juan Rojo <juanrojochacon@gmail.com>
2019-10-28 19:03:35 +0100

    added more examples

---
+++ Tutorials/Tutorial2/Lecture2-LogisticRegression-SUSY.py
@@ -0,0 +1,202 @@
+###############################################################################################
+# In this example we apply Logistic Regression to an LHC dataset
+# Original version from arXiv:1803.08823, Phys.Rept. 810 (2019) 1-124
+# "A high-bias, low-variance introduction to Machine Learning for physicists"
+# P. Mehta, M. Bukov, C.-H. Wang, A. G. R. Day, C. Richardson, C. K. Fisher, D. J. Schwab.
+# Adapted by Juan Rojo, j.rojo@vu.nl
+###############################################################################################
+
+#########################################################
+# Import the relevant libraries
+import numpy as np
+from sklearn import datasets, linear_model
+from sklearn.preprocessing import PolynomialFeatures
+from matplotlib import pyplot as plt, rcParams
+import sys, os
+import pandas as pd
+import warnings
+import tensorflow as tf
+########################################################
+
+##############################################################
+# Import the SUSY dataset with pandas
+#Comment the next line on to turn off warnings
+warnings.filterwarnings('ignore')
+
+print("\n *************** Importing LHC data files ************ \n")
+
+seed=12
+np.random.seed(seed)
+
+# suppress tflow compilation warnings
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+tf.set_random_seed(seed)
+
+# Download the SUSY.csv (about 2GB) from UCI ML archive and save it in the same directory as this jupyter notebook
+# See: https://archive.ics.uci.edu/ml/machine-learning-databases/00279/
+#filename="SUSY.csv"
+filename='/Users/juanrojo/physics19/ML/SUSYdataset/SUSY.csv' 
+
+columns=["signal", "lepton 1 pT", "lepton 1 eta", "lepton 1 phi", "lepton 2 pT", "lepton 2 eta", 
+         "lepton 2 phi", "missing energy magnitude", "missing energy phi", "MET_rel", 
+         "axial MET", "M_R", "M_TR_2", "R", "MT2", "S_R", "M_Delta_R", "dPhi_r_b", "cos(theta_r1)"]
+
+# Load 1,500,000 rows as train data, 50,000 as test data
+df_train=pd.read_csv(filename,names=columns,nrows=1500000,engine='python')
+df_test=pd.read_csv(filename,names=columns,nrows=50000, skiprows=1500000,engine='python')
+#
+
+print("\n *************** LHC data succesfully imported ************ \n")
+
+
+##############################################################
+
+print("\n *************** Run Logistic Regression ************ \n")
+
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import sys
+#import ml_style as style #optional styling sheet
+#mpl.rcParams.update(style.style) #optional styling sheet
+
+def getTrainData(nVar):
+    designMatrix = df_train.iloc[:,1:nVar+1].values
+    #now the signal
+    labels = df_train['signal'].values # labels (0 or 1)
+    return (designMatrix,labels)
+
+def getTestData(nVar):
+    designMatrix = df_test.iloc[:,1:nVar+1].values
+    #now the signal
+    labels = df_test['signal'].values
+    return (designMatrix,labels)
+
+# define
+def build_roc_curve(probs, signal_bit, threshes):
+    # Convert things to a pandas series to build a DataFrame
+    # which will make ROC curve logic easier to express
+    signal_probs = pd.Series(probs[:,1])
+    signal_true = pd.Series(signal_bit)
+    signal_df = pd.DataFrame(signal_probs, columns=['sig_prob'])
+    signal_df.loc[:,'sig_true'] = signal_true
+    Acceptance = []
+    Rejection = []
+    for thresh in threshes:
+        # define acceptance
+        signal_df.loc[:,'accept'] = signal_df['sig_prob'] > thresh
+        # sum over data frame with slicing conditions
+        nSigCor = len(signal_df[(signal_df['accept']) & (signal_df['sig_true']==1.)])
+        nSig = len(signal_df[signal_df['sig_true']==1.])
+        nBkgCor = len(signal_df[ (signal_df['sig_true']==0.) & (~signal_df['accept'])])
+        nBkg = len(signal_df[signal_df['sig_true']==0.])
+        Acceptance.append(nSigCor/nSig) # False positive rate
+        Rejection.append(nBkgCor/nBkg) # True positive rate
+
+    return Acceptance, Rejection
+    
+# let's define this as a function so we can call it easily
+def runTensorFlowRegression(nVar,alpha):
+
+    #make data array placeholder for just first 8 simple features
+    x = tf.placeholder(tf.float32,[None,nVar])
+    #make weights and bias
+    W = tf.Variable(tf.zeros([nVar,2])) #we will make y 'onehot' 0 bit is bkg, 1 bit is signal
+    b = tf.Variable(tf.zeros([2]))
+
+    #make 'answer variable'
+    y = tf.nn.softmax(tf.matmul(x, W) + b)
+    #placeholder for correct answer
+    y_ = tf.placeholder(tf.float32, [None, 2])
+    #cross entropy with L2 regularizer
+    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=y,labels=y_)+alpha*tf.nn.l2_loss(W))
+    
+    #define training step
+    train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+    #initialize variables 
+    init = tf.global_variables_initializer()
+    #setup session
+    sess = tf.Session()
+    sess.run(init)
+
+    #ok now everything is setup for tensorflow, but we need the data in a useful form
+    #first let's get the variables
+    Var_train, Sig_train_bit1 = getTrainData(nVar)
+    #now the signal
+    Sig_train_bit0 = Sig_train_bit1.copy()
+    Sig_train_bit0 = 1 - Sig_train_bit0
+    Sig_train = np.column_stack((Sig_train_bit0,Sig_train_bit1))
+    
+    ######## ------- TRAINING ----------############
+    #Now perform minibatch gradient descent with minibatches of size 100:
+    n_data = len(Sig_train_bit1)
+    minibatch_size = 1000
+    n_minibatch = n_data//minibatch_size
+    print('\t Training with %i minibatches, dataset size is %i'%(n_minibatch,n_data))
+    for i in range(0, n_minibatch):
+        sys.stdout.write("%.3f %% completed \r"%(100*i/n_minibatch))
+        sys.stdout.flush()
+        start = i*minibatch_size
+        end = (i+1)*minibatch_size-1
+        
+        batch_x = Var_train[start:end]
+        batch_y = Sig_train[start:end]
+        sess.run(train_step, feed_dict={x: batch_x, y_: batch_y})
+    
+    
+    # Accuracy function:
+    correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    
+    ######## ------- TESTING ----------############
+    # Setup test data
+    Var_test = df_test.iloc[:,1:nVar+1].values
+    
+    # Now the signal
+    Sig_test_bit1 = df_test['signal'].values
+    
+    Sig_test_bit0 = Sig_test_bit1.copy()
+    Sig_test_bit0 = 1 - Sig_test_bit0
+    Sig_test = np.column_stack((Sig_test_bit0,Sig_test_bit1))
+    print("\t Accuracy for alpha %.1E : %.3f" %(alpha,sess.run(accuracy, feed_dict={x: Var_test, y_: Sig_test})))
+    
+    # Get the weights
+    weights = W.eval(session=sess)
+    # Get probabilities assigned (i.e. evaluate y on test data)
+    probs = y.eval(feed_dict = {x: Var_test}, session = sess)
+    # now let's get the signal efficiency and background rejection on the test data
+    print('\t Computing ROC curve ...')
+    # build ROC curve by scanning over thresholds of probability of being
+    # a background event and calculating signal efficiency/background rejection
+    # at each threshold
+    threshes = np.arange(0,1,0.01)
+    Acceptance, Rejection = build_roc_curve(probs, Sig_test_bit1, threshes)
+
+    return (probs,Acceptance,Rejection)
+
+
+# We sweep the L2 regularizer strength (alpha) over 10 decades
+alphas = np.logspace(-10,-1,11)
+fig = plt.figure()
+ax = fig.add_subplot(111)
+it=0
+for alpha in alphas:
+    print("Training for alpha = %.2E"%alpha)
+    c1 = 1.*( float(it) % 3.)/3.0
+    c2 = 1.*( float(it) % 9.)/9.0
+    c3 = 1.*( float(it) % 27.)/27.0
+    probsSimple,accep,rej = runTensorFlowRegression(8,alpha)
+    ax.scatter(accep,rej,c=[[c1,c2,c3]],label='Alpha: %.1E' %alpha)
+    it+=1
+    
+ax.set_xlabel('signal efficiency')
+ax.set_ylabel('background rejection')
+plt.legend(loc='lower left', fontsize = 'small');
+plt.show()
+
+print("\n ************ ROC curves succesfully evaluated ************** \n")
+
+exit()
+
+############################################################################
